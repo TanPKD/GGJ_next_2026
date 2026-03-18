f01c31685a62af55de8ad869c61d69d4bba859d9
TanPKD <127940473+TanPKD@users.noreply.github.com>
2026-03-18 16:37:00 +0800

    main 最新进度

--- croptails/scripts/globals/data_typs.gd
+++ croptails/scripts/globals/data_typs.gd
@@ -8,3 +8,17 @@ enum Tools {
 	PlantCorn,
 	PlantTomato
 }
+
+enum scene {
+	test_scene_default,
+	test_scene_house_tilemap,
+	test_scene_player,
+	test_scene_tilemap
+}
+
+const SCENE_PATH_DICT: Dictionary = {
+	DataTypes.scene.test_scene_default: "res://scenes/test/test_scene_default.tscn",
+	DataTypes.scene.test_scene_house_tilemap: "res://scenes/test/test_scene_house_tilemap.tscn",
+	DataTypes.scene.test_scene_player: "res://scenes/test/test_scene_player.tscn",
+	DataTypes.scene.test_scene_tilemap: "res://scenes/test/test_scene_tilemap.tscn"
+}
