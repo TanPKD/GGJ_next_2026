81e3927e634f5ba99802809a4fcf556265506404
TanPKD <127940473+TanPKD@users.noreply.github.com>
2026-03-25 21:53:01 +0800

    main更新

--- croptails/scenes/characters/player/tilling_state.gd
+++ croptails/scenes/characters/player/tilling_state.gd
@@ -3,6 +3,7 @@ extends NodeState
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
+
 func _on_process(_delta : float) -> void:
 	pass
 
@@ -17,7 +18,7 @@ func _on_next_transitions() -> void:
 
 
 func _on_enter() -> void:
-	if player.player_direction==Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("tilling_back")
 	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("tilling_right")
@@ -27,7 +28,6 @@ func _on_enter() -> void:
 		animated_sprite_2d.play("tilling_left")
 	else:
 		animated_sprite_2d.play("tilling_front")
-
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
