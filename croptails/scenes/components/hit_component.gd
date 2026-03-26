18381e4e7ba9ae8b98db8ede0cbb4f3bea99a553
TanPKD <127940473+TanPKD@users.noreply.github.com>
2026-03-26 17:39:31 +0800

    main

--- croptails/scenes/components/hit_component.gd
+++ croptails/scenes/components/hit_component.gd
@@ -3,8 +3,3 @@ extends Area2D
 
 @export var current_tool : DataTypes.Tools = DataTypes.Tools.None
 @export var hit_damage : int = 1
-
-signal on_hurt
-
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
